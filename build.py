b91700e6757aa3b2248188762c7f92d38957b45a
Balazs Benics <benicsbalazs@gmail.com>
2019-05-24 00:49:16 +0200

    fix travis ci: propogate pull option

--- build.py
+++ build.py
@@ -6,4 +6,9 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds()
+    updated_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        options['prometheus-cpp:mode'] = 'pull'
+        updated_builds.append([settings, options, env_vars, build_requires])
+    builder.builds = updated_builds
     builder.run()
