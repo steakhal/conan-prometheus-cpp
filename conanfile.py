d911ad951cbe94c38fbcb9b5b1a74bb01de5013e
Balazs Benics <benicsbalazs@gmail.com>
2019-06-12 16:21:17 +0200

    style reformat

--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, shutil
+import os
+import shutil
 from conans import ConanFile, CMake, tools
 from conans.errors import ConanInvalidConfiguration
 from conans.model.version import Version
+
 
 class PrometheusCppConan(ConanFile):
     name = 'prometheus-cpp'
@@ -12,8 +14,7 @@ class PrometheusCppConan(ConanFile):
     license = 'MIT'
     url = 'https://github.com/steakhal/conan-prometheus-cpp'
     homepage = 'https://github.com/jupp0r/prometheus-cpp'
-    description = \
-        'This library aims to enable Metrics-Driven Development for C++ services'
+    description = 'This library aims to enable Metrics-Driven Development for C++ services'
     topics = (
         'conan',
         'prometheus-cpp',
@@ -22,7 +23,7 @@ class PrometheusCppConan(ConanFile):
         'statistics',
         'profile',
         'log',
-        )
+    )
     author = 'Balazs Benics <benicsbalazs@gmail.com>'
     settings = ('os', 'compiler', 'build_type', 'arch')
     options = {
@@ -31,75 +32,55 @@ class PrometheusCppConan(ConanFile):
         'mode': ['pull', 'push'],
         'enable_compression': [True, False],
         'override_cxx_standard_flags': [True, False],
-        }
+    }
     default_options = {
         'shared': False,
         'fPIC': True,
         'enable_compression': True,
         'override_cxx_standard_flags': True,
-        }
+    }
     _source_subfolder = 'source_subfolder'
     _build_subfolder = 'build_subfolder'
     exports = 'LICENSE.md'
     exports_sources = ['CMakeLists.txt', _source_subfolder]
     generators = 'cmake'
 
-    @property
-    def _is_mingw_windows(self):
-        return self.settings.os == 'Windows' and self.settings.compiler == 'gcc' and os.name == 'nt'
-
-    @property
-    def _is_msvc(self):
-        return self.settings.compiler == 'Visual Studio'
-
     def config_options(self):
-        """Remove fPIC option if the compiler is Visual Studio.
-        """
-
         if self.settings.os == 'Windows':
             self.options.remove('fPIC')
 
     def source(self):
-        """Download and unzip the sources than wrap the original CMake file to call conan_basic_setup.
-        """
-
         tools.get('%s/archive/v%s.zip' % (self.homepage, self.version))
         os.rename('prometheus-cpp-%s' % self.version,
                   self._source_subfolder)
         os.rename(os.path.join(self._source_subfolder, 'CMakeLists.txt'
-                  ), os.path.join(self._source_subfolder,
-                  'CMakeListsOriginal.txt'))
+                               ), os.path.join(self._source_subfolder,
+                                               'CMakeListsOriginal.txt'))
         shutil.move('CMakeLists.txt',
                     os.path.join(self._source_subfolder,
-                    'CMakeLists.txt'))
+                                 'CMakeLists.txt'))
 
     def configure(self):
         if self.settings.compiler == "Visual Studio" and \
            Version(self.settings.compiler.version.value) < "14":
-            raise ConanInvalidConfiguration("Prometheus-cpp does not support MSVC < 14")
+            raise ConanInvalidConfiguration(
+                "Prometheus-cpp does not support MSVC < 14")
 
     def requirements(self):
         if self.options.mode == 'pull':
             self.requires('civetweb/1.12.0@steakhal/testing')
             if self.options.enable_compression:
                 self.requires.add('zlib/1.2.11@conan/stable')
-        else: # self.options.mode == 'push':
+        else:  # self.options.mode == 'push':
             self.requires.add('libcurl/7.61.1@bincrafters/stable')
 
     def _configure_cmake(self):
-        """Create CMake instance and execute configure step.
-        """
-
         cmake = CMake(self)
         cmake.definitions['BUILD_SHARED_LIBS'] = self.options.shared
 
         if self.settings.os != 'Windows':
             cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = \
                 self.options.fPIC
-
-        #if self._is_mingw_windows:
-        #    print('it is _is_mingw_windows')
-        #    cmake.definitions['CMAKE_CXX_FLAGS'] = '-fno-use-linker-plugin'
 
         cmake.definitions['ENABLE_PULL'] = self.options.mode == 'pull'
         cmake.definitions['ENABLE_PUSH'] = self.options.mode == 'push'
@@ -116,16 +97,10 @@ class PrometheusCppConan(ConanFile):
         return cmake
 
     def build(self):
-        """Configure, build and install prometheus-cpp using CMake.
-        """
-
         cmake = self._configure_cmake()
         cmake.build()
 
     def package(self):
-        """Installs prometheus-cpp artifacts to package folder
-        """
-
         cmake = self._configure_cmake()
         cmake.install()
         self.copy('LICENSE', dst='licenses', src=self._source_subfolder)
@@ -136,19 +111,15 @@ class PrometheusCppConan(ConanFile):
         self.copy("*.dylib", dst="bin", src="lib")
         self.copy("*.so", dst="bin", src="lib")
 
-    def package_info(self):
+    def package_info(self):    
         if self.options.mode == 'pull':
             self.cpp_info.libs.append('prometheus-cpp-pull')
-        else: #self.options.mode == 'push':
+        else:  # self.options.mode == 'push':
             self.cpp_info.libs.append('prometheus-cpp-push')
-
         self.cpp_info.libs.append("prometheus-cpp-core")
 
         if self.settings.os == 'Linux':
-            self.cpp_info.libs.append('pthread')
-            self.cpp_info.libs.append('rt')
-            if self.options.shared:
-                self.cpp_info.libs.append('dl')
+            self.cpp_info.libs.extend(["dl", "rt", "pthread"])
 
         # gcc's atomic library not linked automatically on clang
         if self.settings.compiler == 'clang':
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os, time, shutil, subprocess, requests, glob
+import os
+import time
+import subprocess
+import requests
 from conans import ConanFile, CMake, tools
+
 
 class PrometheusCppConanTest(ConanFile):
     settings = ('os', 'compiler', 'build_type', 'arch')
@@ -10,22 +14,25 @@ class PrometheusCppConanTest(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions['TEST_PROMETHEUS_PULL'] = (self.options['prometheus-cpp'].mode == 'pull')
+        cmake.definitions['TEST_PROMETHEUS_PULL'] = (
+            self.options['prometheus-cpp'].mode == 'pull')
         cmake.configure()
         cmake.build()
 
     def test(self):
         prometheus_path = self.deps_cpp_info['prometheus-cpp'].rootpath
 
-        prometheus_license    = os.path.join(prometheus_path, 'licenses', 'LICENSE')
-        conan_package_license = os.path.join(prometheus_path, 'licenses', 'LICENSE.md')
+        prometheus_license = os.path.join(
+            prometheus_path, 'licenses', 'LICENSE')
+        conan_package_license = os.path.join(
+            prometheus_path, 'licenses', 'LICENSE.md')
         assert(os.path.exists(prometheus_license))
         assert(os.path.exists(conan_package_license))
 
         if tools.cross_building(self.settings):
             self.output.warn('Cross Building: Skipping Test Package')
             return
-  
+
         if self.options['prometheus-cpp'].mode == 'pull':
             sample_server_path = os.path.join('bin', 'sample_server')
             sample_server = subprocess.Popen([sample_server_path])
@@ -37,8 +44,9 @@ class PrometheusCppConanTest(ConanFile):
                 sample_server.kill()
                 assert(response.ok)
                 assert(-1 != response.text.find('How many seconds is this server running?'))
-        else: # mode == 'push':
-            self.output.warn('Test for push mode is not yet implemented: Skipping Test Package')
+        else:  # mode == 'push':
+            self.output.warn(
+                'Test for push mode is not yet implemented: Skipping Test Package')
             # TODO finish
             return
             sample_client_path = os.path.join('bin', 'sample_client')
